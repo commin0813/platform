1c19ce6d39ae32ffed50c55d7e77f225ff4cc40d
khm0813 <khm0813@hellonms.net>
2016-09-12 09:10:06 +0900

    for문

--- workspace/src/A.java
+++ workspace/src/A.java
@@ -1,10 +1,16 @@
-
 public class A {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("aa");
-			System.out.println("this is git test Class.. !");
+		String[] arr = new String[10];
+
+		for (int i = 0; i < arr.length; i++) {
+				arr[i] = "index : "+(int)(i+Math.random()*100);
+		}
+		
+		for(String str : arr){
+			System.out.println(str);
+		}
 	}
 
 }
