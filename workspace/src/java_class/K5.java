55942847d57991bdd42dc28b183cc703c18d4a31
commin0813 <commin0813@gmail.com>
2016-11-04 14:36:28 +0900

    aa

--- workspace/src/java_class/K5.java
+++ workspace/src/java_class/K5.java
@@ -8,4 +8,18 @@ public class K5 {
 	String getBody(){
 		return "k5_body";
 	}
+	String createCar(){
+		return getBody()+getTire();
+	}
+	public static void main(String [] ar){
+		K5 car = new K5();
+		
+		System.out.println(car.getTire());
+		
+		System.out.println(car.getBody());
+		
+		System.out.println();
+		
+		System.out.println(car.createCar()+"\nNumber:"+car.carNumber);
+	}
 }
