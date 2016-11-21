a0b34c8f570ff254047981352280c3627f1c0132
commin0813 <commin0813@gmail.com>
2016-11-21 13:55:35 +0900

    change name to lotto

---
+++ workspace/src/lotto/LottoMain.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LottoMain {
+	// developed by commin//
+
+	// Lotto Program //
+	// ver 1.0.0 //
+	// 2016.11.8 //
+
+
+	public void start() {
+	
+
+	}
+
+	public static void main(String[] ar) {
+		new LottoMain();// 객체가 딱히 필요없을때는 인스턴스만 만들어줍니다.
+	}
+}
---
+++ workspace/src/lotto/LottoPro.java
@@ -0,0 +1,15 @@
+package lotto;
+
+public class LottoPro {
+
+	// developed by commin//
+
+	// Lotto Program //
+	// ver 1.0.0 //
+	// 2016.11.8 //
+
+	// 전역 변수
+
+	
+
+}
