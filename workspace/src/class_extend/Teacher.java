9934a8778fc637018d5c50c535f42d705b8df1b0
commin0813 <commin0813@gmail.com>
2016-12-12 14:40:29 +0900

    ggg

---
+++ workspace/src/class_extend/Teacher.java
@@ -0,0 +1,14 @@
+package class_extend;
+
+public class Teacher extends School{
+	public Teacher(){
+		System.out.println("반 확인 프로그램");
+	}
+	
+	@Override
+	public String getBan(String name){
+		return super.getBan(name)+"반";
+	}
+	
+
+}
